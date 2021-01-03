6b0c3b61044f2260c64aaf2bd4a3186c61c5c2ca
Alooi <alnasser@purdue.edu>
2021-01-02 22:11:13 -0500

    glitchy af....

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
+import ComfyJS from 'comfy.js';
+import {useState} from 'react';
 import './App.css';
+import tracker from './tracker';
+import {XYPlot, HorizontalBarSeries, AbstractSeries, XAxis, YAxis,LabelSeries} from 'react-vis';
+ComfyJS.Init("aladdintwisted");
+var counter = 0;
+
 
 function App() {
+  var data = []
+  const [message, setMessage] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [counters, setcounters] = useState({})
+  ComfyJS.onChat = (user, message) => {
+    setCounter(counter + 1);
+    setMessage(message);
+    tracker(message,counters,setcounters);
+  }
+  function getHighest(){
+    var items = Object.keys(counters).map(function(key) {
+      return [key, counters[key]];
+    });
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+    // console.log(items.slice(0, 5));
+    items = items.slice(0, 5)
+    for (var i = 0; i < items.length; i++){
+      data[i] = {x: items[i][1], y: i, label: items[i][0]}
+      // console.log(items[i][0]);
+    }
+    // return (items)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <text>enter your user name</text>
+        <p>{message}</p>
+        <XYPlot height={400} width={600}>
+          <HorizontalBarSeries data={data}/>
+          <LabelSeries data={data} animation/>
+          <XAxis/>
+        </XYPlot>
+        <p>{getHighest()}</p>
+        <button onClick={()=>{setcounters({})}}>reset</button>
       </header>
+      
     </div>
   );
 }
---
+++ src/tracker.js
@@ -0,0 +1,19 @@
+function tracker(message,getcounters,setcounters){
+   var counters = getcounters;
+   const limit = 100;
+      var words = message.split(" ");
+      var tempWords = [];
+      for (var i = 0; i < words.length; i++){
+         if (!counters[words[i]]){
+            counters[words[i]] = 0;
+         }
+         if (!tempWords.includes(words[i])) {
+            counters[words[i]] += 1;
+            tempWords.push(words[i]);
+         }
+      }
+   setcounters(counters);
+}
+
+
+export default tracker;
